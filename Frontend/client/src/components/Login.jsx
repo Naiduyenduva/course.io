62f191b0e2c68edb9fc279b9d166d677d8d6d37b
Laxmunaidu <luckyyenduva8.7@gmail.com>
2024-11-11 18:15:01 +0530

    login added

--- Frontend/client/src/components/Login.jsx
+++ Frontend/client/src/components/Login.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleEmailChange = (e) => {
+      setEmail(e.target.value)
+  }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+ }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/user/signin',{
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify({email,password})
+      });
+      
+      if(response.ok) {
+        alert('logged in successfully');
+      } else {
+        throw new Error ('invalid credentials')
+      }
+      const data = await response.json();
+      localStorage.setItem('token',data.token)
+      
+    } catch(err) {
+      setError(err.message)
+    }
+  }
+
   return (
-    <div>login</div>
+    <>
+    <div>
+      <form onSubmit={handleSubmit}>
+          <input 
+          type='text'
+          name='email'
+          required={true}
+          value={email}
+          onChange={handleEmailChange}
+          /><br/>
+          <input 
+          type='password'
+          name='password'
+          required={true}
+          value={password}
+          onChange={handlePasswordChange}
+          /><br/>
+          <button>Login</button>
+      </form>
+    </div>
+  </>
   )
 }
 
