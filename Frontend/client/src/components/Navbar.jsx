ce66214a3003d8d4799da82d4b9bc94972bcb36e
Laxmunaidu <luckyyenduva8.7@gmail.com>
2024-11-16 15:54:04 +0530

    adding responsive

--- Frontend/client/src/components/Navbar.jsx
+++ Frontend/client/src/components/Navbar.jsx
@@ -13,20 +13,20 @@ export const Navbar = ({isLoggedIn,setIsLoggedIn}) => {
   return (
     <>
         <div className='flex justify-between bg-black h-20 pt-4 text-center gap-20 text-xl font-semibold border border-black border-b-slate-900'>
-          <div className='flex gap-2 ml-20'>
-          <Link to='/'><BookOpenText color='white' className='mt-2'/></Link>
-            <h1 className='text-blue-200 font-bold text-2xl'>Course.io</h1>
+          <div className='flex gap-2 lg:ml-20'>
+            <Link to='/'><BookOpenText color='white' className='mt-2 ml-5'/></Link>
+            <h1 className='text-blue-200 font-bold text-2xl lg:text-2xl'>Course.io</h1>
           </div>
           {isLoggedIn ? (
                <>
               <Link to='/allCourses'> <h1 className='p-2 h-10 rounded-lg text-sm bg-blue-600 text-white mr-10'>All Courses</h1></Link>
               <Link to='/purchasedCourses'> <h1 className='p-2 h-10 rounded-lg text-sm bg-blue-600 text-white mr-10'>Purchased Courses</h1></Link>
-               <Link to='/'><button onClick={logout} className='p-2 h-10 rounded-lg text-sm w-20 bg-blue-600 text-white mr-10'>Logout </button> </Link> 
+               <Link to='/'><button onClick={logout} className='p-2 h-10 rounded-lg text-sm w-14 lg:w-20 bg-blue-600 text-white mr-10'>Logout </button> </Link> 
                </>
             ) : (
           <div className='flex justify-center gap-5 mr-20'>
-            <Link to='/signup'><button className='p-2 h-10 rounded-lg text-sm w-20 bg-blue-600 text-white'>Signup</button></Link>
-            <Link to='login'><button className='p-2 w-20 h-10 bg-blue-600 rounded-lg text-white text-sm'>Login</button></Link>
+            <Link to='/signup'><button className='p-2 h-10 rounded-lg text-sm w-14 lg:w-20 bg-blue-600 text-white'>Signup</button></Link>
+            <Link to='login'><button className='p-2 w-14 lg:w-20 h-10 bg-blue-600 rounded-lg text-white text-sm'>Login</button></Link>
           </div>
                 )}
         </div>
